f6fda6dabfec609ac10d32753296f63c2b50ae2e
Hervé Kimbau <hervekimbau2021@gmail.com>
2021-11-21 09:36:49 +0100

    v2 test toujurs

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
 <body>
     <h1>Le premier test</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde, iure nulla accusamus quidem facilis quas natus quasi enim alias, temporibus officiis dolores, distinctio ad ipsa nesciunt id molestias debitis et.</p>
+    <h1>Deuxieme test</h1>
+    <p style="color: red;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde quae nihil distinctio molestiae, quos iste nostrum placeat quo dolore tenetur rerum, provident praesentium consectetur numquam id dolor cumque sint alias?</p>
 </body>
 
 </html>
