947224a30f17d1fe85b2fbb038c02ff9b3f0c25b
Hervé Kimbau <edgardkimbau@gmail.com>
2021-11-21 09:07:33 +0100

    v1 titre & para

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Exercice git</title>
+</head>
+
+<body>
+    <h1>Le premier test</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde, iure nulla accusamus quidem facilis quas natus quasi enim alias, temporibus officiis dolores, distinctio ad ipsa nesciunt id molestias debitis et.</p>
+</body>
+
+</html>
